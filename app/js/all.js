b24496f98393f3242810fecfb4f533423e924649
Kilowat <kadantsev.sergey@gmail.com>
2017-07-08 23:59:07 +0300

    init

--- app/js/all.js
+++ app/js/all.js
@@ -1,2 +1,9 @@
 
-$(document).ready(function(){$('.gallery .bxslider').bxSlider({pagerCustom:'.gallery .bx-pager',captions:true,});$('.selectize').selectize();});
+$(document).ready(function(){$('.gallery .bxslider').bxSlider({pagerCustom:'.gallery .bx-pager',captions:true,});$('.selectize').selectize();$('.selectize-grey').selectize();;(function($,window,document,undefined)
+{$('.inputfile').each(function()
+{var $input=$(this),$label=$input.next('label'),labelVal=$label.html();$input.on('change',function(e)
+{var fileName='';if(this.files&&this.files.length>1)
+fileName=(this.getAttribute('data-multiple-caption')||'').replace('{count}',this.files.length);else if(e.target.value)
+fileName=e.target.value.split('\\').pop();if(fileName)
+$label.find('span').html(fileName);else
+$label.html(labelVal);});$input.on('focus',function(){$input.addClass('has-focus');}).on('blur',function(){$input.removeClass('has-focus');});});})(jQuery,window,document);});
